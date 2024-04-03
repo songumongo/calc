b9d14811e9eba3054859a92a2541a628c3c38472
songumongo <160731472+songumongo@users.noreply.github.com>
2024-04-03 08:10:19 -0300

    Add files via upload

---
+++ calca.js
@@ -0,0 +1,27 @@
+const n1 = 2
+const n2 = 2
+
+function soma(){
+    console.log(n1+n2);
+
+}
+soma();
+
+function sub(){
+    console.log(n1-n2);
+
+}
+sub();
+
+function prod(){
+    console.log(n1*n2);
+
+}
+prod();
+
+function div(){
+    console.log(n1/n2);
+
+}
+
+div();
